c45541cfd7924a416db3a9aa1dea1e0b817ccbb3
Omar Faruk Sharif <omar@omar-sharif.net>
2016-03-03 12:30:50 +0600

    Create AttributeController.php

---
+++ app/code/community/Omar/ProductByAttribute/controllers/AttributeController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Omar_ProductByAttribute_AttributeController extends Mage_Core_Controller_Front_Action
+{
+
+    public $_block;
+
+
+    //  prodattr/attribute/index/manufacturer/6/?dir=asc&order=price
+    public function indexAction()
+    {
+        $this->loadLayout();
+
+        $this->_block = Mage::getBlockSingleton('omar_productbyattribute/list');
+
+        if( empty( $this->_block->getAttr() ) || empty( $this->_block->getAttrValue() ) ) {
+            return Mage::app()->getResponse()->setRedirect(Mage::getBaseUrl());
+        }
+
+        $title = $this->_block->getPageTitle();
+
+        if(!empty($title)) {
+            $this->getLayout()->getBlock('head')->setTitle( $title );
+        }
+
+        $this->_block->getBreadcrumb();
+
+        $this->renderLayout();
+    }
+
+
+}
